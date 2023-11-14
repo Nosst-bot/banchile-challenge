e1a68177c8748424a7c2aef9818d4c025da50939
Kevin <kevsalvatierrafs@gmail.com>
2023-11-14 11:46:29 -0300

    Integracion de Cliente y Cuenta, creacion de util para generar numero random de cuenta(no implementado)

--- src/main/java/com/pacadmin/banchilechallenge/model/Cliente.java
+++ src/main/java/com/pacadmin/banchilechallenge/model/Cliente.java
@@ -1,4 +1,19 @@
 package com.pacadmin.banchilechallenge.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clientes")
 public class Cliente {
+
+    @Column(name = "id_cliente")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCliente;
+
+    @Column(name = "rut")
+    private String rut;
+
+    @Column(name = "nombre")
+    private String nombre;
 }
--- src/main/java/com/pacadmin/banchilechallenge/model/Cuenta.java
+++ src/main/java/com/pacadmin/banchilechallenge/model/Cuenta.java
@@ -1,4 +1,29 @@
 package com.pacadmin.banchilechallenge.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "cuentas")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cuenta {
+
+    @Column(name = "id_cuenta")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCuenta;
+
+    @Column(name = "nombre_banco")
+    private String nombreBanco;
+
+
+    @Column(name = "id_cliente")
+    private Long idCliente;
 }
