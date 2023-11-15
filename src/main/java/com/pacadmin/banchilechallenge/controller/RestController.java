c34ac2963955842888d296d9b619fc4500ff8727
SrMule <eduardo.garciao@usm.cl>
2023-11-15 03:11:58 -0300

    crossorigin

--- src/main/java/com/pacadmin/banchilechallenge/controller/RestController.java
+++ src/main/java/com/pacadmin/banchilechallenge/controller/RestController.java
@@ -4,10 +4,13 @@ import com.pacadmin.banchilechallenge.services.GlobalServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.web.bind.annotation.RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET})
 public class RestController {
 
     @Autowired
