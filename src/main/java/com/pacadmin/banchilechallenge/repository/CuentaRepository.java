4ef3f9f16546b7706b62e4352a05d5fa19515e85
SrMule <eduardo.garciao@usm.cl>
2023-11-14 21:02:46 -0300

    actualizacion filtros repository

--- src/main/java/com/pacadmin/banchilechallenge/repository/CuentaRepository.java
+++ src/main/java/com/pacadmin/banchilechallenge/repository/CuentaRepository.java
@@ -4,7 +4,11 @@ import com.pacadmin.banchilechallenge.model.Cuenta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+
 
 @Repository
 public interface CuentaRepository extends JpaRepository<Cuenta, Long> {
+
+    List<Cuenta> findByCuentaNombreBanco(String cuentaNombreBanco);
 }
--- src/main/java/com/pacadmin/banchilechallenge/repository/PACRepository.java
+++ src/main/java/com/pacadmin/banchilechallenge/repository/PACRepository.java
@@ -3,7 +3,14 @@ package com.pacadmin.banchilechallenge.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.pacadmin.banchilechallenge.model.PAC;
+import java.util.List;
+import java.util.Date;
+
 
 @Repository
 public interface PACRepository extends JpaRepository<PAC,Long> {
+
+    List<PAC> findByPacFecha(Date pacFecha);
+    List<PAC> findByPacMontoBetween(Double montoInicial, Double montoFinal);
+
 }
